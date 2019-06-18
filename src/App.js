eb7f2d9df439e4a5cc80df9ed228519e5c54105b
elinahovakimyan <elinahovakimyan@gmail.com>
2019-06-18 14:10:24 +0400

    Update GA

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactGA from 'react-ga';
 
 import Home from './pages/Home';
 import Splash from './pages/Splash';
@@ -12,10 +11,6 @@ class App extends React.PureComponent {
     isLoading: true,
   }
 
-  componentWillMount() {
-    this.initializeReactGA();
-  }
-
   componentDidMount() {
     this.timer = setTimeout(() => {
       this.setState({ isLoading: false })
@@ -26,10 +21,6 @@ class App extends React.PureComponent {
     if (this.timer) {
       clearTimeout(this.timer);
     }
-  }
-
-  initializeReactGA = () => {
-    ReactGA.initialize('UA-109245198-1');
   }
 
   render() {
