1ae5459c11dd58be3f7cb615c2ffadb706032d81
Elina Hovakimyan <elinahovakimyan@gmail.com>
2020-04-03 20:21:44 +0400

    Update portfolio

--- src/pages/FreelancerHome/components/PortfolioSection/data.js
+++ src/pages/FreelancerHome/components/PortfolioSection/data.js
@@ -1,54 +1,92 @@
 const data = [
   {
-    name:"prudost",  
-    title:"Goal Setting App",
-    technologies:"React Native (iOS & Android), Django (backend)"
+    name: "prudost",
+    title: "Goal Setting App",
+    technologies: "React Native (iOS & Android), Django (backend)"
   },
   {
-    name:"zenapillar",
-    title:"Service Publishing App",
-    technologies:"React Native (iOS & Android)",
-    link:"https://apps.apple.com/us/app/zenapillar/id1481114066"
+    name: "zenapillar",
+    title: "Service Publishing App",
+    technologies: "React Native (iOS & Android)",
+    link: "https://apps.apple.com/us/app/zenapillar/id1481114066"
   },
   {
-    name:"prehab101",
-    title:"Fitness App",
-    technologies:"React Native (iOS & Android)",
+    name: "prehab101",
+    title: "Fitness App",
+    technologies: "React Native (iOS & Android)",
   },
   {
-    name:"veratize",
-    title:"Property Access App",
-    technologies:"React Native (iOS & Android)",
+    name: "veratize",
+    title: "Property Access App",
+    technologies: "React Native (iOS & Android)",
   },
   {
-    name:"master8lc",
-    title:"Bar Reservation App",
-    technologies:"React Native (iOS & Android)",
-    link:"https://apps.apple.com/us/app/id1479139549"
+    name: "master8lc",
+    title: "Bar Reservation App",
+    technologies: "React Native (iOS & Android)",
+    link: "https://apps.apple.com/us/app/id1479139549"
   },
   {
-    name:"the_up",
-    title:"Social Platform",
-    technologies:"React Native (iOS & Android)",
-    link:"https://apps.apple.com/us/app/the-up/id1483478217"
+    name: "the_up",
+    title: "Social Platform",
+    technologies: "React Native (iOS & Android)",
+    link: "https://apps.apple.com/us/app/the-up/id1483478217"
   },
   {
-    name:"patani",
-    title:"Educational App for Kids",
-    technologies:"React Native (Android)",
+    name: "patani",
+    title: "Educational App for Kids",
+    technologies: "React Native (Android)",
     // link:"https://play.google.com/store/apps/details?id=com.ngo.pataniapp"
   },
   {
-    name:"react-native-month-picker",
-    title:"react-native-month-picker",
-    technologies:"Open-source project",
-    link:"https://npmjs.com/package/react-native-month-picker"
+    hasGif: true,
+    name: "bitcoinbot",
+    title: "Bitcoinbot Pro Website",
+    technologies: "React.js, Redux, LESS",
+    link: "https://bitcoinbot.pro"
   },
   {
-    name:"react-recaptcha-google",
-    title:"react-recaptcha-google",
-    technologies:",Open-source project",
-    link:"https://npmjs.com/package/react-recaptcha-google"
+    name: "securitytrails",
+    title: "Security Trails Website",
+    technologies: "React.js, Redux, CSS",
+    link: "https://securitytrails.com"
+  },
+  {
+    name: "eventor",
+    title: "Eventor - Event Planning Platform",
+    technologies: "React.js, Redux, SCSS",
+    link: "https://eventor.am"
+  },
+  {
+    name: "deegeehub",
+    title: "Deegeehub Website",
+    technologies: "Wordpress",
+    link: "https://deegeehub.com"
+  },
+  {
+    name: "prudost_web",
+    title: "Prudost Landing Page",
+    technologies: "Wordpress",
+    link: "https://prudost.com"
+  },
+  {
+    hasGif: true,
+    name: "tiamo",
+    title: "Tiamo Jewelry Website",
+    technologies: "Wordpress, Woocommerce",
+    link: "https://tiamo-jewelry.com"
+  },
+  {
+    name: "react-native-month-picker",
+    title: "react-native-month-picker",
+    technologies: "Open-source project",
+    link: "https://npmjs.com/package/react-native-month-picker"
+  },
+  {
+    name: "react-recaptcha-google",
+    title: "react-recaptcha-google",
+    technologies: "Open-source project",
+    link: "https://npmjs.com/package/react-recaptcha-google"
   },
 ];
 
